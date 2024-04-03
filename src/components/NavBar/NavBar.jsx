471e624bde037a396882b0cc56a90fdee2313ac6
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-04-03 11:43:36 +0600

    Navber add

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,17 @@
+import { NavLink } from "react-router-dom";
 
 
 const NavBar = () => {
+  
+    const Navlink =<>
+         <li><NavLink to="/">Home</NavLink></li>
+         <li><NavLink to="/login">Login</NavLink></li>
+         <li><NavLink to="/register">registration</NavLink></li>
+  
+  
+    </>
+
+
   return (
     <div className="navbar bg-base-100">
   <div className="navbar-start">
@@ -9,16 +20,8 @@ const NavBar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
+        {Navlink}
+        </ul>
     </div>
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
@@ -29,8 +32,7 @@ const NavBar = () => {
         <details>
           <summary>Parent</summary>
           <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+          {Navlink}
           </ul>
         </details>
       </li>
