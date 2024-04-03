2d3894d9856325feb5e1517080419460afb3fc46
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-04-03 11:36:39 +0600

    Navber Add

--- src/components/Route/Route.jsx
+++ src/components/Route/Route.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
 
 
 const Route = () => {
   return (
     <div>
+        <NavBar></NavBar>
         <Outlet></Outlet>
     </div>
   );
